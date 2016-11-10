f3fcd870059c3f3045926339751928bf2f24148f
Joel Byler <jbyler@covermymeds.com>
2016-11-10 11:20:40 -0500

    Add active column to Users index

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -7,7 +7,7 @@ defmodule Pairmotron.UserControllerTest do
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, user_path(conn, :index)
-    assert html_response(conn, 200) =~ "Listing users"
+    assert html_response(conn, 200) =~ "Users"
   end
 
   test "renders form for new resources", %{conn: conn} do
