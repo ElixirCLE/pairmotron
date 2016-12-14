db239fd9c3fe089a75f723aea13d537a4e773ae4
Eric Workman <ericsparkeworkman+github@gmail.com>
2016-12-14 14:17:40 -0500

    Revert "Merge pull request #45 from ElixirCLE/revert-40-39-add-guardian"
    This reverts commit 4235ebe6c309e06e26a4eb39da601a0e5b952194, reversing
    changes made to 7d161596f20e2c81684d3347f612ce13ab05370b.

--- web/controllers/authenticate.ex
+++ web/controllers/authenticate.ex
@@ -2,7 +2,7 @@ defmodule Pairmotron.Plug.Authenticate do
   alias Pairmotron.Router.Helpers, as: Routes
   import Plug.Conn
 
-  alias Pairmotron.{Repo, User}
+  alias Pairmotron.User
 
   def init(opts) do
     opts
@@ -13,23 +13,13 @@ defmodule Pairmotron.Plug.Authenticate do
   end
 
   defp assign_current_user(conn = %Plug.Conn{}) do
-    current_user_id = current_assigned_user_id(conn) || get_session(conn, :current_user_id)
-    assign_current_user(conn, current_user_id)
-  end
-  defp assign_current_user(conn, id) when is_integer(id) do
-    assign_current_user(conn, Repo.get(User, id))
+    current_user = conn.assigns[:current_user] || Guardian.Plug.current_resource(conn)
+    assign_current_user(conn, current_user)
   end
   defp assign_current_user(conn, user = %User{}) do
     assign(conn, :current_user, user)
   end
   defp assign_current_user(conn, _), do: redirect_to_sign_in(conn)
-
-  defp current_assigned_user_id(conn) do
-    case user = conn.assigns[:current_user] do
-      %User{} -> user.id
-      _ -> nil
-    end
-  end
 
   defp redirect_to_sign_in(conn) do
     conn
--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -9,34 +9,40 @@ defmodule Pairmotron.SessionController do
     render conn, changeset: User.changeset(%User{})
   end
 
-  def create(conn, %{"user" => user_params}) do
-    if is_nil(user_params["email"]) do
-      nil
-    else
-      Repo.get_by(User, email: user_params["email"])
-    end
-    |> sign_in(user_params["password"], conn)
+  def create(conn, %{"user" => %{"email" => nil}}) do
+    conn |> bad_sign_in
   end
 
+  def create(conn, %{"user" => user_params = %{"email" => email}}) do
+    Repo.get_by(User, email: email)
+    |> sign_in(user_params, conn)
+  end
+
+  def create(conn, _params), do: bad_sign_in(conn)
+
   def delete(conn, _) do
-    delete_session(conn, :current_user_id)
+    Guardian.Plug.sign_out(conn)
     |> put_flash(:info, "You have been logged out")
     |> redirect(to: session_path(conn, :new))
   end
 
   @sign_in_error "Name and/or password are incorrect."
 
-  defp sign_in(user, password, conn) when is_nil(user) or is_nil(password) do
+  defp bad_sign_in(conn) do
     conn
     |> put_flash(:error, @sign_in_error)
     |> render("new.html", changeset: User.changeset(%User{}))
   end
 
-  defp sign_in(user, password, conn) when is_map(user) do
+  defp sign_in(_user, %{"password" => nil}, conn) do
+    conn |> bad_sign_in
+  end
+
+  defp sign_in(user, %{"password" => password}, conn) do
     if Comeonin.Bcrypt.checkpw(password, user.password_hash) do
       conn
-      |> put_session(:current_user_id, user.id)
       |> put_flash(:info, "You are now signed in.")
+      |> Guardian.Plug.sign_in(user)
       |> redirect(to: page_path(conn, :index))
     else
       conn
@@ -44,4 +50,6 @@ defmodule Pairmotron.SessionController do
       |> render("new.html", changeset: User.changeset(%User{}))
     end
   end
+
+  defp sign_in(_user, _params, conn), do: bad_sign_in(conn)
 end
