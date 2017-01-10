186813f63aeac85edfd7d688143ab4f18e4f47a1
Mathew Bramson <mathewbramson@gmail.com>
2017-01-09 20:05:40 -0500

    Refactor page controller tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -9,61 +9,55 @@ defmodule Pairmotron.PageControllerTest do
     assert redirected_to(conn) == session_path(conn, :new)
   end
 
-  test "lists one active user", %{conn: conn} do
-    user = insert(:user)
-    conn = conn
-      |> log_in(user)
-      |> get("/pairs")
-    assert html_response(conn, 200) =~ user.name
-  end
+  describe "while authenticated" do
+    setup do
+      user = insert(:user)
+      conn = build_conn
+        |> log_in(user)
+      {:ok, [conn: conn, logged_in_user: user]}
+    end
 
-  test "does not list an inactive user", %{conn: conn} do
-    user = insert(:user, active: false)
-    conn = conn
-      |> log_in(user)
-      |> get("/pairs")
-    refute html_response(conn, 200) =~ user.name
-  end
+    test "lists one active user", %{conn: conn, logged_in_user: user} do
+      conn = get(conn, "/pairs")
+      assert html_response(conn, 200) =~ user.name
+    end
 
-  test "pairs two users together", %{conn: conn} do
-    [user1, user2] = insert_pair(:user)
-    conn = conn
-      |> log_in(user1)
-      |> get("/pairs")
-    assert html_response(conn, 200) =~ user1.name
-    assert html_response(conn, 200) =~ user2.name
-  end
+    test "does not list an inactive user", %{conn: conn} do
+      user = insert(:user, active: false)
+      conn = get(conn, "/pairs")
+      refute html_response(conn, 200) =~ user.name
+    end
 
-  test "does not re-pair after the first pair has been made", %{conn: conn} do
-    paired_users = [user1, user2] = insert_pair(:user)
-    create_pair(paired_users)
-    new_user = insert(:user)
-    conn = conn
-      |> log_in(new_user)
-      |> get("/pairs")
-    assert html_response(conn, 200) =~ user1.name
-    assert html_response(conn, 200) =~ user2.name
-    refute html_response(conn, 200) =~ new_user.name
-  end
+    test "pairs two users together", %{conn: conn, logged_in_user: user1} do
+      user2 = insert(:user)
+      conn = get(conn, "/pairs")
+      assert html_response(conn, 200) =~ user1.name
+      assert html_response(conn, 200) =~ user2.name
+    end
 
-  test "does not pairify for a week that is not current", %{conn: conn} do
-    [user1, user2] = insert_pair(:user)
-    conn = log_in(conn, user1)
-    conn = get conn, page_path(conn, :show, 1999, 1)
-    refute html_response(conn, 200) =~ user1.name
-    refute html_response(conn, 200) =~ user2.name
-  end
+    test "displays link to retro :create for a pair that has no retrospective" do
+    end
 
-  test "repairifying deletes current pairs and redirects to show", %{conn: conn} do
-    {year, week} = Timex.iso_week(Timex.today)
-    paired_users = [user1, user2] = insert_pair(:user)
-    create_pair(paired_users)
+    test "does not re-pair after the first pair has been made", %{conn: conn, logged_in_user: user} do
+      create_pair([user])
+      new_user = insert(:user)
+      conn = get(conn, "/pairs")
+      assert html_response(conn, 200) =~ user.name
+      refute html_response(conn, 200) =~ new_user.name
+    end
 
-    conn = log_in(conn, user1)
-    conn = delete conn, page_path(conn, :delete, year, week)
-    assert redirected_to(conn) == page_path(conn, :show, year, week)
+    test "does not pairify for a week that is not current", %{conn: conn, logged_in_user: user} do
+      conn = get conn, page_path(conn, :show, 1999, 1)
+      refute html_response(conn, 200) =~ user.name
+    end
 
-    refute Repo.get_by(UserPair, %{user_id: user1.id})
-    refute Repo.get_by(UserPair, %{user_id: user2.id})
+    test "repairifying deletes current pairs and redirects to show", %{conn: conn, logged_in_user: user} do
+      {year, week} = Timex.iso_week(Timex.today)
+      create_pair([user])
+      conn = delete conn, page_path(conn, :delete, year, week)
+      assert redirected_to(conn) == page_path(conn, :show, year, week)
+      refute Repo.get_by(UserPair, %{user_id: user.id})
+    end
+
   end
 end
