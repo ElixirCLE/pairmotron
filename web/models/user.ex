df8705282c9661212fce9c5a6cdeaf48869c4c13
Eric Workman <ericsparkeworkman+github@gmail.com>
2017-01-24 17:13:45 -0500

    Remove optional changeset is_admin

--- web/models/user.ex
+++ web/models/user.ex
@@ -19,7 +19,7 @@ defmodule Pairmotron.User do
   @minimum_password_length 8
 
   @required_params ~w(name email)
-  @optional_params ~w(password password_confirmation active is_admin)
+  @optional_params ~w(password password_confirmation active)
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
