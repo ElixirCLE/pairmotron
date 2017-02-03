665660400cbd00fe076e3e335380ee28a7ded345
Eric Workman <ericsparkeworkman+github@gmail.com>
2017-02-03 00:53:41 -0500

    Allow the Pairer to understand previous pairs and try to naively match lonely new pairs

--- lib/pairmotron/pairer.ex
+++ lib/pairmotron/pairer.ex
@@ -1,24 +1,47 @@
+defmodule PairerResult do
+  defstruct user_pair: nil, pairs: []
+end
+
 defmodule Pairmotron.Pairer do
+
+  alias Pairmotron.UserPair
+
+  def generate_pairs(users, []), do: generate_pairs(users)
+  def generate_pairs(users, user_pairs) do
+    users
+      |> chunk
+      |> unlonelify(user_pairs)
+  end
 
   def generate_pairs(users) do
     users
-    |> Enum.chunk(2, 2, [])
-    |> unlonelify
+      |> chunk
+      |> unlonelify
+  end
+
+  defp chunk(users) do
+    users
+      |> Enum.chunk(2, 2, [])
   end
 
   defp unlonelify(pairs) do
-    pairs
-    |> Enum.reverse
-    |> friendify
-    |> Enum.reverse
+    results = pairs
+      |> Enum.reverse
+      |> friendify
+      |> Enum.reverse
+    %PairerResult{pairs: results}
   end
 
-  defp friendify( pairs = [ [_first, _second] | _rest] ), do: pairs
-
-  defp friendify( [ [single] | [ pair | rest ] ] ) do
-    [[single | pair] | rest ]
+  defp unlonelify([], _), do: %PairerResult{}
+  defp unlonelify(pairs = [[ _f, _s]], _), do: %PairerResult{pairs: pairs}
+  defp unlonelify([[single]], [user_pair | _user_pairs]) do
+    %PairerResult{user_pair: UserPair.changeset(%UserPair{}, %{pair_id: user_pair.pair_id, user_id: single.id})}
   end
 
+  defp friendify(pairs = [ [_first, _second] | _rest]), do: pairs
+  defp friendify([[single] | [pair | rest]]) do
+    [[single | pair] | rest]
+  end
   defp friendify(pairs), do: pairs
 
 end
--- test/lib/pairer_test.exs
+++ test/lib/pairer_test.exs
@@ -1,8 +1,7 @@
 defmodule Pairmotron.PairerTest do
   use ExUnit.Case, async: true
 
-  alias Pairmotron.Pairer
-  alias Pairmotron.User
+  alias Pairmotron.{Pairer, User, UserPair}
 
   @user_1 %User{id: 1}
   @user_2 %User{id: 2}
@@ -10,35 +9,62 @@ defmodule Pairmotron.PairerTest do
   @user_4 %User{id: 4}
   @user_5 %User{id: 5}
 
-  describe ".generate_pairs" do
+  @user_pair_1 %UserPair{pair_id: 1, user_id: 1}
+  @user_pair_2 %UserPair{pair_id: 1, user_id: 2}
+  @user_pair_3 %UserPair{pair_id: 2, user_id: 3}
+  @user_pair_4 %UserPair{pair_id: 2, user_id: 4}
+
+  @user_pair_changeset UserPair.changeset(%UserPair{}, %{pair_id: 2, user_id: 5})
+
+  describe ".generate_pairs/1" do
     test "empty list of users returns empty list" do
-      assert Pairer.generate_pairs([]) == []
+      assert %PairerResult{pairs: []} = Pairer.generate_pairs([])
     end
 
     test "list of one user returns a pair of that user" do
-      assert Pairer.generate_pairs( [@user_1] ) == [ [@user_1] ]
+      assert %PairerResult{pairs: [[@user_1]]} == Pairer.generate_pairs([@user_1])
     end
 
     test "list of two users returns a pair of those users" do
-      assert [first_pair] = Pairer.generate_pairs([@user_1, @user_2])
+      assert %PairerResult{pairs: [first_pair]} = Pairer.generate_pairs([@user_1, @user_2])
       assert Enum.sort(first_pair) == [@user_1, @user_2]
     end
 
     test "list of four users returns two pairs of those users" do
-      assert [first_pair, second_pair] = Pairer.generate_pairs([@user_1, @user_2, @user_3, @user_4])
+      assert %PairerResult{pairs: [first_pair, second_pair]} = Pairer.generate_pairs([@user_1, @user_2, @user_3, @user_4])
       assert Enum.sort(first_pair) == [@user_1, @user_2]
       assert Enum.sort(second_pair) == [@user_3, @user_4]
     end
 
     test "list of three users returns one pair of those three users" do
-      assert [first_pair] = Pairer.generate_pairs([@user_1, @user_2, @user_3])
+      assert %PairerResult{pairs: [first_pair]} = Pairer.generate_pairs([@user_1, @user_2, @user_3])
       assert Enum.sort(first_pair) == [@user_1, @user_2, @user_3]
     end
 
     test "list of five users returns two pairs with the three person pair at the end" do
-      assert [first_pair, second_pair] = Pairer.generate_pairs([@user_1, @user_2, @user_3, @user_4, @user_5])
+      assert %PairerResult{pairs: [first_pair, second_pair]} = Pairer.generate_pairs([@user_1, @user_2, @user_3, @user_4, @user_5])
       assert Enum.sort(first_pair) == [@user_1, @user_2]
       assert Enum.sort(second_pair) == [@user_3, @user_4, @user_5]
+    end
+  end
+
+  describe ".generate_pairs/2" do
+    test "empty list of users and empty user pairs returns empty list" do
+      assert %PairerResult{user_pair: nil, pairs: []} = Pairer.generate_pairs([], [])
+    end
+
+    test "some users and an empty list of user pairs pairs the users" do
+      assert %PairerResult{pairs: [first_pair]} = Pairer.generate_pairs([@user_1, @user_2], [])
+      assert Enum.sort(first_pair) == [@user_1, @user_2]
+    end
+
+    test "new users with existing pairs pairs the new users" do
+      assert %PairerResult{pairs: [first_pair]} = Pairer.generate_pairs([@user_3, @user_4], [@user_pair_1, @user_pair_2])
+      assert Enum.sort(first_pair) == [@user_3, @user_4]
+    end
+
+    test "single new user with exist pair gets placed with the pair" do
+      assert %PairerResult{user_pair: @user_pair_changeset} = Pairer.generate_pairs([@user_5], [@user_pair_3, @user_pair_4])
     end
   end
 end
