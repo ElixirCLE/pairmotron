4dffb5a57698b9ff3a43e819ace13488dea5fc86
Mathew Bramson <mathewbramson@gmail.com>
2017-02-03 10:46:03 -0500

    Refactor lib/abilities

--- lib/abilities.ex
+++ lib/abilities.ex
@@ -9,26 +9,25 @@ defimpl Canada.Can, for: Pairmotron.User do
   def can?(%User{id: user_id}, action, %Group{owner_id: user_id})
     when action in [:edit, :update, :delete], do: true
 
-  def can?(%User{id: user_id}, :show, project = %Project{}) do
-    if is_nil(project.group_id) do
-      project = project |> Pairmotron.Repo.preload([{:pair_retros, :user}])
-      project.pair_retros
-      |> Enum.any?(fn retro -> retro.user.id == user_id end)
-    else
-      project = project |> Pairmotron.Repo.preload([{:group, :users}])
-      project.group.users
-      |> Enum.any?(fn user -> user.id == user_id end)
-    end
+  def can?(%User{id: user_id}, :show, project = %Project{group_id: nil}) do
+    project = project |> Pairmotron.Repo.preload([{:pair_retros, :user}])
+    project.pair_retros
+    |> Enum.any?(fn retro -> retro.user.id == user_id end)
   end
+
+  def can?(%User{id: user_id}, :show, project = %Project{}) do
+    project = project |> Pairmotron.Repo.preload([{:group, :users}])
+    project.group.users
+    |> Enum.any?(fn user -> user.id == user_id end)
+  end
+
+  def can?(%User{}, action, %Project{group_id: nil})
+    when action in [:edit, :update, :delete], do: false
 
   def can?(%User{id: user_id}, action, project = %Project{})
     when action in [:edit, :update, :delete] do
-    if is_nil(project.group_id) do
-      false
-    else
-      project = project |> Pairmotron.Repo.preload(:group)
-      project.group.owner_id == user_id
-    end
+    project = project |> Pairmotron.Repo.preload(:group)
+    project.group.owner_id == user_id
   end
 
   def can?(%User{}, _, _), do: false
