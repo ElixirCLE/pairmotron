4db34d86b323171898ff234abc8bc9ced4d8a8d3
Mathew Bramson <mathewbramson@gmail.com>
2017-02-15 09:47:32 -0500

    Fix migration

--- priv/repo/migrations/20170206182420_create_group_membership_request.exs
+++ priv/repo/migrations/20170206182420_create_group_membership_request.exs
@@ -2,13 +2,15 @@ defmodule Pairmotron.Repo.Migrations.CreateGroupMembershipRequest do
   use Ecto.Migration
 
   def change do
-    create table(:group_membership_request) do
+    create table(:group_membership_requests) do
       add :user_id, references(:users, on_delete: :delete_all), null: false
       add :group_id, references(:groups, on_delete: :delete_all), null: false
       add :initiated_by_user, :boolean, null: false
 
       timestamps()
     end
+
+    create unique_index(:group_membership_requests, [:user_id, :group_id])
 
   end
 end
