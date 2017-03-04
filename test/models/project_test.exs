b88eee47919492f077d98f0d099782b81a2cba86
Mathew Bramson <mathewbramson@gmail.com>
2017-03-04 15:01:50 -0500

    Add projects_for_group/1

--- test/models/project_test.exs
+++ test/models/project_test.exs
@@ -15,4 +15,21 @@ defmodule Pairmotron.ProjectTest do
     changeset = Project.changeset(%Project{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  describe "projects_for_group" do
+    test "returns nothing when there are no projects" do
+      assert Project.projects_for_group(1) |> Repo.one == nil
+    end
+
+    test "returns a project that is associated with the given group" do
+      group = insert(:group)
+      project = insert(:project, %{group: group})
+      assert Repo.one(Project.projects_for_group(group.id)).id == project.id
+    end
+
+    test "does not return a project that is not associated with the given group" do
+      project = insert(:project)
+      assert Project.projects_for_group(1) |> Repo.one == nil
+    end
+  end
 end
