628518121aa7003cd2763d77b4674f3315b66fba
Eric Workman <eworkman@covermymeds.com>
2017-03-07 16:40:36 -0500

    Add specs and use conn correctly

--- web/controllers/group_pair_controller.ex
+++ web/controllers/group_pair_controller.ex
@@ -4,6 +4,7 @@ defmodule Pairmotron.GroupPairController do
   import Pairmotron.ControllerHelpers
   alias Pairmotron.{PairMaker, Group}
 
+  @spec show(%Plug.Conn{}, map()) :: %Plug.Conn{}
   def show(conn, %{"year" => y, "week" => w, "id" => g}) do
     {year, _} = y |> Integer.parse
     {week, _} = w |> Integer.parse
@@ -11,13 +12,7 @@ defmodule Pairmotron.GroupPairController do
 
     group = Repo.get(Group, group_id)
     if authorized?(group, conn.assigns.current_user) do
-      pairs = case PairMaker.fetch_or_gen(year, week, group_id) do
-        {:error, pairs, message} ->
-          conn |> put_flash(:error, message)
-          pairs
-        {:ok, pairs} ->
-          pairs
-      end
+      {conn, pairs} = fetch_pairs(conn, year, week, group_id)
       conn = assign_current_user_pair_retro_for_week(conn, year, week)
       render conn, "index.html", pairs: pairs, year: year, week: week, group: group,
         start_date: Timex.from_iso_triplet({year, week, 1}),
@@ -31,14 +26,14 @@ defmodule Pairmotron.GroupPairController do
     show(conn, %{"id" => g, "year" => year |> Integer.to_string, "week" => week |> Integer.to_string})
   end
 
+  @spec delete(%Plug.Conn{}, map()) :: %Plug.Conn{}
   def delete(conn, %{"year" => y, "week" => w, "id" => g}) do
     if conn.assigns.current_user.is_admin do
       {year, _} = y |> Integer.parse
       {week, _} = w |> Integer.parse
       {group_id, _} = g |> Integer.parse
-      PairMaker.generate_pairs(year, week, group_id)
       conn
-        |> put_flash(:info, "Repairified")
+        |> generate_pairs(year, week, group_id)
         |> redirect(to: group_pair_path(conn, :show, group_id, year, week))
     else
       redirect_not_authorized(conn, group_pair_path(conn, :show, g, y, w))
@@ -49,5 +44,23 @@ defmodule Pairmotron.GroupPairController do
     group = group |> Pairmotron.Repo.preload(:users)
     group.users
       |> Enum.any?(fn guser -> guser.id == user.id end)
+  end
+
+  defp fetch_pairs(conn, year, week, group_id) do
+    case PairMaker.fetch_or_gen(year, week, group_id) do
+      {:error, pairs, message} ->
+        {conn |> put_flash(:error, message), pairs}
+      {:ok, pairs} ->
+        {conn, pairs}
+    end
+  end
+
+  defp generate_pairs(conn, year, week, group_id) do
+    case PairMaker.generate_pairs(year, week, group_id) do
+      {:error, message} ->
+        conn |> put_flash(:error, message)
+      {:ok, _} ->
+        conn |> put_flash(:info, "Repairified")
+    end
   end
 end
