6a22d40f0d6d9616321e3921b0c07f2e0f37534f
Mathew Bramson <mathewbramson@gmail.com>
2017-03-07 17:31:22 -0500

    Fix Group credo issues
    Add moduledoc to Group
    
    Add TypeSpecs to Group

--- web/models/group.ex
+++ web/models/group.ex
@@ -1,4 +1,14 @@
 defmodule Pairmotron.Group do
+  @moduledoc """
+  Groups contain users and allow certain aspects of Pairmotron to be siloed on
+  a per group basis.
+
+  Groups have their own set of pairs, and Users can be in pairs of multiple
+  Groups at the same time. Groups also have their own set of projects.
+
+  Groups also have their own configuration options for how users are paired.
+  (Not yet implemented)
+  """
   use Pairmotron.Web, :model
 
   schema "groups" do
@@ -18,6 +28,7 @@ defmodule Pairmotron.Group do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
+  @spec changeset(map() | %Ecto.Changeset{}, map()) :: %Ecto.Changeset{}
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, @required_fields, @optional_fields)
@@ -28,6 +39,7 @@ defmodule Pairmotron.Group do
   Builds a changeset and sets the users association. Should be used
   when creating a group so that the owner can be set as the only user.
   """
+  @spec changeset_for_create(map() | %Ecto.Changeset{}, map(), list(Types.user)) :: %Ecto.Changeset{}
   def changeset_for_create(struct, params \\ %{}, users) do
     struct
     |> cast(params, @required_fields, @optional_fields)
@@ -35,6 +47,7 @@ defmodule Pairmotron.Group do
     |> put_assoc(:users, users)
   end
 
+  @spec groups_for_user(Types.user) :: %Ecto.Query{}
   def groups_for_user(user) do
     from group in Pairmotron.Group,
     join: u in assoc(group, :users),
