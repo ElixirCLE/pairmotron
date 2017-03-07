932d9c89e2b26326b2fc2fd9feacf0e36eb9791a
Mathew Bramson <mathewbramson@gmail.com>
2017-03-07 17:29:56 -0500

    Add Type aliases for Ecto Schemas

---
+++ lib/pairmotron/types.ex
@@ -0,0 +1,11 @@
+defmodule Pairmtron.Types do
+  @moduledoc false
+
+  # Ecto Schema Types
+  @type user :: Pairmotron.User
+  @type group :: Pairmotron.Group
+  @type retro :: Pairmotron.PairRetro
+  @type pair :: Pairmotron.Pair
+  @type project :: Pairmotron.Project
+  @type user_group :: Pairmotron.UserGroup
+end
