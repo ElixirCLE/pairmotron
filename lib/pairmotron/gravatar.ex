6b6103e5fc2845ed81e57d5a376a5e661cf4f7e4
Mathew Bramson <mathewbramson@gmail.com>
2017-03-07 22:18:55 -0500

    Fix Pairmotron.Gravatar credo issues
    Add moduledoc, TypeSpec and function Doc
    
    Fix raw pipe chain start

--- lib/pairmotron/gravatar.ex
+++ lib/pairmotron/gravatar.ex
@@ -1,5 +1,16 @@
 defmodule Pairmotron.Gravatar do
+  @moduledoc """
+  Handles the integration with Gravatar. Used by templates to retrieve the
+  proper gravatar image URL for a given user.
+  """
+
+  @doc """
+  Given an email, returns the URL for the Gravatar avatar associated with that
+  email.
+  """
+  @spec url(email :: String.t) :: String.t
   def url(email) do
-    "https://gravatar.com/avatar/" <> (:crypto.hash(:md5, email) |> Base.encode16(case: :lower))
+    md5_email = :crypto.hash(:md5, email)
+    "https://gravatar.com/avatar/" <> (md5_email |> Base.encode16(case: :lower))
   end
 end
