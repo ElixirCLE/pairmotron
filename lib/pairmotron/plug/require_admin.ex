a241aec8554c219ef807d3ce0f447f6e025a961b
Mathew Bramson <mathewbramson@gmail.com>
2017-03-07 22:58:45 -0500

    Fix Pairmotron.Plug.RequireAdmin credo issues
    Add moduledoc and TypeSpecs

--- lib/pairmotron/plug/require_admin.ex
+++ lib/pairmotron/plug/require_admin.ex
@@ -1,18 +1,26 @@
 defmodule Pairmotron.Plug.RequireAdmin do
+  @moduledoc """
+  Plug which halts the Plug.Conn and redirects to the /pairs path if the logged
+  in user is not an admin.
+  """
   import Plug.Conn
 
   alias Pairmotron.Router.Helpers, as: Routes
   alias Pairmotron.Authentication, as: Auth
+  alias Pairmotron.Types
 
+  @spec init(keyword()) :: keyword()
   def init(opts), do: opts
 
+  @spec call(%Plug.Conn{}, keyword()) :: %Plug.Conn{}
   def call(conn, _opts) do
     current_user = Auth.current_user(conn)
     require_admin(current_user, conn)
   end
 
-  def require_admin(%{is_admin: true}, conn), do: conn
-  def require_admin(_, conn) do
+  @spec require_admin(Types.user, %Plug.Conn{}) :: %Plug.Conn{}
+  defp require_admin(%{is_admin: true}, conn), do: conn
+  defp require_admin(_, conn) do
     conn
     |> Phoenix.Controller.redirect(to: Routes.pair_path(conn, :index))
     |> halt
