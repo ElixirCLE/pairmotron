accd099d925b2f8e0bbcd4a2ab7c856b14c934c2
Mathew Bramson <mathewbramson@gmail.com>
2017-03-07 22:48:06 -0500

    Fix ViewHelper credo issues
    Add moduledoc and TypeSpecs
    
    Refactor value_from_changeset to remove the possibility of it throwing
    an error by passing :error to elem(1).

--- web/views/view_helper.ex
+++ web/views/view_helper.ex
@@ -1,10 +1,17 @@
 defmodule Pairmotron.ViewHelpers do
+  @moduledoc """
+  Contains various functions that are used by a number of views/templates. Auto
+  imported into views and templates through web.es.
+  """
+  alias Pairmotron.Types
+
   @doc """
   Given a user, returns true if that user has a role and that role's
   is_admin property is true. Errors if the user's role is not loaded
   and the user has a role. This is to prevent calling the database in
   a view.
   """
+  @spec is_admin?(Types.user) :: boolean()
   def is_admin?(user) do
     user.is_admin
   end
@@ -13,7 +20,13 @@ defmodule Pairmotron.ViewHelpers do
   Given a changeset and a field that whose value is needed, returns
   that value and only that value. Useful for hidden fields in forms.
   """
+  @spec value_from_changeset(%Ecto.Changeset{}, atom()) :: term()
   def value_from_changeset(changeset, field) do
-    elem(Ecto.Changeset.fetch_field(changeset, field), 1)
+    changeset
+    |> Ecto.Changeset.fetch_field(field)
+    |> case do
+      {_, value} -> value
+      _ -> "error"
+    end
   end
 end
