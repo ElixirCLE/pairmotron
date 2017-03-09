fa4f4b7d8e45c72304b67c4fad9df4b35585897a
Mathew Bramson <mathewbramson@gmail.com>
2017-03-08 20:40:51 -0500

    Fix SessionController credo issues
    Add modulespecs, and TypeSpecs

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,10 +1,14 @@
 defmodule Pairmotron.SessionController do
+  @moduledoc """
+  Handles users logging in and logging out.
+  """
   use Pairmotron.Web, :controller
 
   alias Pairmotron.User
 
   plug :scrub_params, "user" when action in [:create]
 
+  @spec new(%Plug.Conn{}, map()) :: %Plug.Conn{}
   def new(conn, _params) do
     if Guardian.Plug.current_resource(conn) do
       conn |> redirect(to: pair_path(conn, :index))
@@ -13,18 +17,18 @@ defmodule Pairmotron.SessionController do
     end
   end
 
+  @spec create(%Plug.Conn{}, map()) :: %Plug.Conn{}
   def create(conn, %{"user" => %{"email" => nil}}) do
     conn |> bad_sign_in
   end
-
   def create(conn, %{"user" => user_params = %{"email" => email}}) do
     User
     |> Repo.get_by(email: email)
     |> sign_in(user_params, conn)
   end
-
   def create(conn, _params), do: bad_sign_in(conn)
 
+  @spec delete(%Plug.Conn{}, map()) :: %Plug.Conn{}
   def delete(conn, _) do
     conn
     |> Guardian.Plug.sign_out
@@ -34,12 +38,14 @@ defmodule Pairmotron.SessionController do
 
   @sign_in_error "Name and/or password are incorrect."
 
+  @spec bad_sign_in(%Plug.Conn{}) :: %Plug.Conn{}
   defp bad_sign_in(conn) do
     conn
     |> put_flash(:error, @sign_in_error)
     |> render("new.html", changeset: User.changeset(%User{}))
   end
 
+  @spec sign_in(any(), map(), %Plug.Conn{}) :: %Plug.Conn{}
   defp sign_in(nil, _, conn), do: bad_sign_in(conn)
   defp sign_in(_, %{"password" => nil}, conn), do: bad_sign_in(conn)
   defp sign_in(user, %{"password" => password}, conn) do
@@ -54,6 +60,5 @@ defmodule Pairmotron.SessionController do
       |> render("new.html", changeset: User.changeset(%User{}))
     end
   end
-
   defp sign_in(_user, _params, conn), do: bad_sign_in(conn)
 end
