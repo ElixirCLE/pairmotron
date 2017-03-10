155e05f6da876375a12e1d6b211dcb9d0c050c74
Mathew Bramson <mathewbramson@gmail.com>
2017-03-10 13:17:50 -0500

    Add Typespecs to ExAdmin Authorization

--- web/admin/authorization.ex
+++ web/admin/authorization.ex
@@ -1,10 +1,12 @@
 alias Pairmotron.Authentication, as: Auth
 alias Pairmotron.Authorization, as: Authz
 
-
 defimpl ExAdmin.Authorization, for: Pairmotron.User do
+  @spec authorize_query(any(), %Plug.Conn{}, %Ecto.Query{}, any(), any()) :: %Ecto.Query{}
   def authorize_query(resource, conn, query, _action, _id),
     do: Authz.authorize_user_query(resource, Auth.current_user(conn), query)
+
+  @spec authorize_action(any(), %Plug.Conn{}, atom()) :: boolean()
   def authorize_action(_resource, conn, action),
     do: Authz.authorize_actions(action, Auth.current_user(conn),
           except: [:create, :new, :destroy, :delete])
