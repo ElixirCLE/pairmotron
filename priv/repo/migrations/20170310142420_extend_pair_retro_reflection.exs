d6934d83714a55a6b064178134fdca00a931a09e
Eric Workman <eworkman@covermymeds.com>
2017-03-10 12:51:48 -0500

    Change migration to separate up/down

--- priv/repo/migrations/20170310142420_extend_pair_retro_reflection.exs
+++ priv/repo/migrations/20170310142420_extend_pair_retro_reflection.exs
@@ -1,10 +1,15 @@
 defmodule Pairmotron.Repo.Migrations.ExtendPairRetroReflection do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:pair_retros) do
       modify :reflection, :text
     end
+  end
 
+  def down do
+    alter table(:pair_retros) do
+      modify :reflection, :string
+    end
   end
 end
