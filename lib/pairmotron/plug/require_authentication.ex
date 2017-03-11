bf14d37ee7b61001e9ab492ae3e30dedd40a699a
Mathew Bramson <mathewbramson@gmail.com>
2017-03-11 12:48:05 -0500

    Add Typespecs to RequireAuthentication Plug

--- lib/pairmotron/plug/require_authentication.ex
+++ lib/pairmotron/plug/require_authentication.ex
@@ -4,23 +4,29 @@ defmodule Pairmotron.Plug.RequireAuthentication do
 
   alias Pairmotron.User
 
+  @spec init(keyword()) :: keyword()
   def init(opts) do
     opts
   end
 
+  @spec call(%Plug.Conn{}, keyword()) :: %Plug.Conn{}
   def call(conn, _opts) do
     assign_current_user(conn)
   end
 
+  @spec assign_current_user(%Plug.Conn{}) :: %Plug.Conn{}
   defp assign_current_user(conn = %Plug.Conn{}) do
     current_user = conn.assigns[:current_user] || Guardian.Plug.current_resource(conn)
     assign_current_user(conn, current_user)
   end
+
+  @spec assign_current_user(%Plug.Conn{}, any()) :: %Plug.Conn{}
   defp assign_current_user(conn, user = %User{}) do
     assign(conn, :current_user, user)
   end
   defp assign_current_user(conn, _), do: redirect_to_sign_in(conn)
 
+  @spec redirect_to_sign_in(%Plug.Conn{}) :: %Plug.Conn{}
   defp redirect_to_sign_in(conn) do
     conn
     |> Phoenix.Controller.put_flash(
@@ -30,5 +36,4 @@ defmodule Pairmotron.Plug.RequireAuthentication do
     |> Phoenix.Controller.redirect(to: Routes.session_path(conn, :new))
     |> halt
   end
-
 end
