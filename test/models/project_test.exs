7a56a600aa437f735b9bf250bf56d66d75fd27e6
Eric Workman <eworkman@covermymeds.com>
2017-03-15 16:32:44 -0400

    Use sanitizer in project model

--- test/models/project_test.exs
+++ test/models/project_test.exs
@@ -6,14 +6,124 @@ defmodule Pairmotron.ProjectTest do
   @valid_attrs %{name: "some content", url: "http://example.org"}
   @invalid_attrs %{url: "nothing"}
 
-  test "changeset with valid attributes" do
-    changeset = Project.changeset(%Project{}, @valid_attrs)
-    assert changeset.valid?
+  describe "changeset" do
+    test "changeset with valid attributes" do
+      changeset = Project.changeset(%Project{}, @valid_attrs)
+      assert changeset.valid?
+    end
+
+    test "changeset with invalid attributes" do
+      changeset = Project.changeset(%Project{}, @invalid_attrs)
+      refute changeset.valid?
+    end
+
+    test "changset sanitizes name" do
+      changeset = Project.changeset(%Project{}, %{name: "<h1>name</h1>"})
+      assert changeset.valid?
+      assert "name" == changeset.changes.name
+    end
+
+    test "changset sanitizes description" do
+      changeset = Project.changeset(%Project{}, %{name: "test", description: "<p>description</p>"})
+      assert changeset.valid?
+      assert "description" == changeset.changes.description
+    end
+
+    test "changset sanitizes url" do
+      changeset = Project.changeset(%Project{}, %{name: "test", url: "http://<script>example</script>.com"})
+      assert changeset.valid?
+      assert "http://example.com" == changeset.changes.url
+    end
   end
 
-  test "changeset with invalid attributes" do
-    changeset = Project.changeset(%Project{}, @invalid_attrs)
-    refute changeset.valid?
+  describe "changeset_for_create" do
+    test "changeset with valid attributes" do
+      user = insert(:user)
+      group = insert(:group, %{owner: user, users: [user]})
+      attrs = Map.merge(@valid_attrs, %{group_id: group.id, created_by_id: user.id})
+      changeset = Project.changeset_for_create(%Project{}, attrs, [group])
+      assert changeset.valid?
+    end
+
+    test "changeset with invalid attributes" do
+      user = insert(:user)
+      group = insert(:group, %{owner: user, users: [user]})
+      attrs = Map.merge(@invalid_attrs, %{group_id: group.id, created_by_id: user.id})
+      changeset = Project.changeset_for_create(%Project{}, attrs, [group])
+      refute changeset.valid?
+    end
+
+    test "changset sanitizes name" do
+      user = insert(:user)
+      group = insert(:group, %{owner: user, users: [user]})
+      attrs = Map.merge(@valid_attrs, %{group_id: group.id, created_by_id: user.id, name: "<h1>name</h1>"})
+      changeset = Project.changeset_for_create(%Project{}, attrs, [group])
+      assert changeset.valid?
+      assert "name" == changeset.changes.name
+    end
+
+    test "changset sanitizes description" do
+      user = insert(:user)
+      group = insert(:group, %{owner: user, users: [user]})
+      attrs = Map.merge(@valid_attrs, %{group_id: group.id, created_by_id: user.id, description: "<p>description</p>"})
+      changeset = Project.changeset_for_create(%Project{}, attrs, [group])
+      assert changeset.valid?
+      assert "description" == changeset.changes.description
+    end
+
+    test "changset sanitizes url" do
+      user = insert(:user)
+      group = insert(:group, %{owner: user, users: [user]})
+      attrs = Map.merge(@valid_attrs, %{group_id: group.id, created_by_id: user.id, url: "http://<script>example</script>.com"})
+      changeset = Project.changeset_for_create(%Project{}, attrs, [group])
+      assert changeset.valid?
+      assert "http://example.com" == changeset.changes.url
+    end
+  end
+
+  describe "changeset_for_update" do
+    test "changeset with valid attributes" do
+      user = insert(:user)
+      group = insert(:group, %{owner: user, users: [user]})
+      attrs = Map.merge(@valid_attrs, %{group_id: group.id, created_by_id: user.id})
+      changeset = Project.changeset_for_update(%Project{}, attrs)
+      assert changeset.valid?
+    end
+
+    test "changeset with invalid attributes" do
+      user = insert(:user)
+      group = insert(:group, %{owner: user, users: [user]})
+      attrs = Map.merge(@invalid_attrs, %{group_id: group.id, created_by_id: user.id})
+      changeset = Project.changeset_for_update(%Project{}, attrs)
+      refute changeset.valid?
+    end
+
+    test "changset sanitizes name" do
+      user = insert(:user)
+      group = insert(:group, %{owner: user, users: [user]})
+      attrs = Map.merge(@valid_attrs, %{group_id: group.id, created_by_id: user.id, name: "<h1>name</h1>"})
+      changeset = Project.changeset_for_update(%Project{}, attrs)
+      assert changeset.valid?
+      assert "name" == changeset.changes.name
+    end
+
+    test "changset sanitizes description" do
+      user = insert(:user)
+      group = insert(:group, %{owner: user, users: [user]})
+      attrs = Map.merge(@valid_attrs, %{group_id: group.id, created_by_id: user.id, description: "<p>description</p>"})
+      changeset = Project.changeset_for_update(%Project{}, attrs)
+      assert changeset.valid?
+      assert "description" == changeset.changes.description
+    end
+
+    test "changset sanitizes url" do
+      user = insert(:user)
+      group = insert(:group, %{owner: user, users: [user]})
+      attrs = Map.merge(@valid_attrs, %{group_id: group.id, created_by_id: user.id, url: "http://<script>example</script>.com"})
+      changeset = Project.changeset_for_update(%Project{}, attrs)
+      assert changeset.valid?
+      assert "http://example.com" == changeset.changes.url
+    end
   end
 
   describe "projects_for_group" do
