824fc1cbd1f63e115fe2b275227e2cd09ef2ec5e
Mathew Bramson <mathewbramson@gmail.com>
2017-03-20 21:35:02 -0400

    Refactor PairController
    Change the name of fetch_pairs to fetch_groups_and_pairs
    
    Instead of passing the conn deep into fetch_groups_and_pairs and
    pairs_for_user_groups, pass up the messages so that they can be attached
    to the conn at the shallowest function in the controller.
    
    Unpipe a few reduce calls to reduce indentation and make the code a bit
    clearer.
    
    Eliminate some variable declarations that were necessary because we were
    passing the conn down.

--- web/controllers/pair_controller.ex
+++ web/controllers/pair_controller.ex
@@ -11,11 +11,13 @@ defmodule Pairmotron.PairController do
   def index(conn, _params) do
     {year, week} = Timex.iso_week(Timex.today)
     user = conn.assigns[:current_user]
-    {conn, groups_and_pairs} = conn
-      |> fetch_pairs(year, week, user)
-    render conn, "index.html", year: year, week: week, groups_and_pairs: groups_and_pairs,
-      start_date: Timex.from_iso_triplet({year, week, 1}),
-      stop_date: Timex.from_iso_triplet({year, week, 7})
+    {groups_and_pairs, messages} = fetch_groups_and_pairs(year, week, user)
+
+    conn
+    |> flash_messages(messages)
+    |> render("index.html", year: year, week: week, groups_and_pairs: groups_and_pairs,
+                start_date: Timex.from_iso_triplet({year, week, 1}),
+                stop_date: Timex.from_iso_triplet({year, week, 7}))
   end
 
   @spec show(%Plug.Conn{}, map()) :: %Plug.Conn{}
@@ -23,43 +25,48 @@ defmodule Pairmotron.PairController do
     {year, _} = y |> Integer.parse
     {week, _} = w |> Integer.parse
     user = conn.assigns[:current_user]
-    {conn, groups_and_pairs} = conn
-      |> fetch_pairs(year, week, user)
-    render conn, "index.html", year: year, week: week, groups_and_pairs: groups_and_pairs,
-      start_date: Timex.from_iso_triplet({year, week, 1}),
-      stop_date: Timex.from_iso_triplet({year, week, 7})
+    {groups_and_pairs, messages} = fetch_groups_and_pairs(year, week, user)
+
+    conn
+    |> flash_messages(messages)
+    |> render("index.html", year: year, week: week, groups_and_pairs: groups_and_pairs,
+                start_date: Timex.from_iso_triplet({year, week, 1}),
+                stop_date: Timex.from_iso_triplet({year, week, 7}))
   end
 
-  @spec fetch_pairs(%Plug.Conn{}, integer(), 1..53, Types.user) :: {%Plug.Conn{}, [{Types.group, Types.pair}]}
-  defp fetch_pairs(conn, year, week, user) do
-    user = user
-      |> Repo.preload(:groups)
-    {conn, pairs} = user
-      |> pairs_for_user_groups(conn, year, week)
-    {conn, Enum.zip(user.groups, pairs)}
+  @spec fetch_groups_and_pairs(integer(), 1..53, Types.user) :: {[{Types.group, Types.pair}], [String.t]}
+  defp fetch_groups_and_pairs(year, week, user) do
+    user = user |> Repo.preload(:groups)
+    {pairs, messages} = pairs_for_user_groups(user, year, week)
+    {Enum.zip(user.groups, pairs), messages}
   end
 
-  @spec pairs_for_user_groups(Types.user, %Plug.Conn{}, integer(), 1..53) :: {%Plug.Conn{}, [Types.pair]}
-  defp pairs_for_user_groups(user, conn, year, week) do
-    conn_and_pairs = {conn, []}
-    user.groups
-      |> Enum.reduce(conn_and_pairs, fn(group, conn_and_pairs) ->
-        {conn, result_pairs} = conn_and_pairs
+  @spec pairs_for_user_groups(Types.user, integer(), 1..53) :: {[Types.pair], [String.t]}
+  defp pairs_for_user_groups(user, year, week) do
+    Enum.reduce(user.groups, {[], []},
+      fn(group, pairs_and_messages) ->
+        {result_pairs, messages} = pairs_and_messages
         case PairMaker.fetch_or_gen(year, week, group.id) do
           {:error, pairs, message} ->
-            {conn |> put_flash(:error, message), result_pairs ++ [pairs |> pairs_containing_user(user)]}
+            {result_pairs ++ [pairs |> pairs_containing_user(user)], [message | messages]}
           {:ok, pairs} ->
-            {conn, result_pairs ++ [pairs |> pairs_containing_user(user)]}
+            {result_pairs ++ [pairs |> pairs_containing_user(user)], [messages]}
         end
       end)
   end
 
   @spec pairs_containing_user([Types.pair], Types.user) :: [Types.pair]
   defp pairs_containing_user(pairs, user) do
-    pairs
-      |> Enum.filter(fn(pair) ->
-           member_ids = pair.users |> Enum.map(&(&1.id))
-           Enum.member?(member_ids, user.id)
-         end)
+    Enum.filter(pairs, fn(pair) ->
+      member_ids = pair.users |> Enum.map(&(&1.id))
+      Enum.member?(member_ids, user.id)
+    end)
+  end
+
+  @spec flash_messages(Plug.Conn.t, [String.t]) :: Plug.Conn.t
+  defp flash_messages(conn, messages) do
+    Enum.reduce(messages, conn, fn message, conn ->
+      put_flash(conn, :error, message)
+    end)
   end
 end
