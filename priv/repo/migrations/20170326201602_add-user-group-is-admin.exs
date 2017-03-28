dd6af3cc11cf8fa5903c6a4209ed4380fb1ca4e3
Mathew Bramson <mathewbramson@gmail.com>
2017-03-27 22:07:30 -0400

    Fix overcomplicated silly migration

--- priv/repo/migrations/20170326201602_add-user-group-is-admin.exs
+++ priv/repo/migrations/20170326201602_add-user-group-is-admin.exs
@@ -1,21 +1,9 @@
 defmodule :"Elixir.Pairmotron.Repo.Migrations.Add-user-group-is-admin" do
   use Ecto.Migration
 
-  def up do
-    alter table(:users_groups) do
-      add :is_admin, :boolean
-    end
-
-    execute "UPDATE users_groups SET is_admin = FALSE where is_admin IS NULL"
-
+  def change do
     alter table (:users_groups) do
-      modify :is_admin, :boolean, null: false, default: false
-    end
-  end
-
-  def down do
-    alter table(:users_groups) do
-      remove :is_admin
+      add :is_admin, :boolean, null: false, default: false
     end
   end
 end
