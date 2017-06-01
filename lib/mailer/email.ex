c09da9dfa617d6eeb82f2288c6616e3f30c0522b
Mathew Bramson <mathewbramson@gmail.com>
2017-06-01 00:13:45 -0400

    Add Pairmotron.Email
    Add Pairmotron.Email.password_reset_email

---
+++ lib/mailer/email.ex
@@ -0,0 +1,46 @@
+defmodule Pairmotron.Email do
+  import Bamboo.Email
+
+  alias Pairmotron.{Types, Endpoint}
+  import Pairmotron.Router.Helpers
+
+  @doc """
+  Returns an email ready to be sent by Bamboo to the user associated with the
+  given password_reset_token. The email contains a link to the
+  PasswordResetController's :edit form where the user can set a new password.
+
+  The password_reset_token's :user relation must be preloaded.
+  """
+  @spec password_reset_email(Types.password_reset_token) :: Bamboo.Email.t
+  def password_reset_email(password_reset_token)  do
+    new_email()
+    |> to(password_reset_token.user.email)
+    |> from("no-reply@pairmotron.com")
+    |> subject("Pairmotron Password Reset")
+    |> html_body(password_reset_email_html_body(password_reset_token.token))
+    |> text_body(password_reset_email_text_body(password_reset_token.token))
+  end
+
+  @spec password_reset_email_html_body(String.t) :: String.t
+  defp password_reset_email_html_body(token_string) do
+    """
+    It looks like you've requested a password reset.
+
+    <a href=#{reset_path(token_string)}>Click to reset your password</a>
+    """
+  end
+
+  @spec password_reset_email_text_body(String.t) :: String.t
+  defp password_reset_email_text_body(token_string) do
+    """
+    It looks like you've requested a passwrod reset.
+
+    Follow this link to reset your password: #{reset_path(token_string)}
+    """
+  end
+
+  @spec reset_path(String.t) :: String.t
+  defp reset_path(token_string) do
+    password_reset_url(Pairmotron.Endpoint, :edit, token_string)
+  end
+end
