8f4b787515ba663c84f49e22121b2086277dc47f
Mathew Bramson <mathewbramson@gmail.com>
2017-06-29 22:46:19 -0400

    Add Email for Inviting a User to a Group

--- test/lib/mailer/email_test.exs
+++ test/lib/mailer/email_test.exs
@@ -33,4 +33,42 @@ defmodule Pairmotron.EmailTest do
         password_reset_url(Pairmotron.Endpoint, :edit, token.token)
     end
   end
+
+  describe "group_invitation_email/2" do
+    setup do
+      user = build(:user)
+      group = build(:group)
+      {:ok, [user: user, group: group]}
+    end
+
+    test "returns an email to the user that is passed in", %{user: user, group: group} do
+      assert Email.group_invitation_email(user, group).to == user.email
+    end
+
+    test "returns an email from no-reply@pairmotron.com", %{user: user, group: group} do
+      assert Email.group_invitation_email(user, group).from == "no-reply@pairmotron.com"
+    end
+
+    test "returns an email with a subject of 'Pairmotron Group Invitation", %{user: user, group: group} do
+      assert Email.group_invitation_email(user, group).subject == "Pairmotron Group Invitation"
+    end
+
+    test "returns an email containing the group name in the html_body", %{user: user, group: group} do
+      assert Email.group_invitation_email(user, group).html_body =~ group.name
+    end
+
+    test "returns an email containing the group name in the text_body", %{user: user, group: group} do
+      assert Email.group_invitation_email(user, group).text_body =~ group.name
+    end
+
+    test "returns an email containing a link to the invitations index in html_body", %{user: user, group: group} do
+      assert Email.group_invitation_email(user, group).html_body =~
+        users_group_membership_request_url(Pairmotron.Endpoint, :index)
+    end
+
+    test "returns an email containing a link to the invitations index in text_body", %{user: user, group: group} do
+      assert Email.group_invitation_email(user, group).text_body =~
+        users_group_membership_request_url(Pairmotron.Endpoint, :index)
+    end
+  end
 end
